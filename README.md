7a3d0dc10ed5d245fbff0d292edce31cb0601a37
Elonisme <3120190971352@stu.xhu.edu.cn>
2022-04-23 17:55:26 +0800

    v1.2

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 3. 打开命令窗口输入 `python3 readjson.py`
 4. 回车即可
 5. 或者也可以使用其他的方式运行python文件，只要能运行即可
+6. 运行完毕后提供两种编码的csv文件，请根据自己需要选择
 ## 相关库
 请确保自己的python库中有pandas，否则将无法运行文件
 下载pandas的命令`pip3 install pandas`
--- readjson.py
+++ readjson.py
@@ -7,25 +7,15 @@
 import pandas as pd
 
 df = pd.read_json(r'.\data\adicted-data.json')
-
-# print(type(df))
-# print(df.keys())
-
 dict = df['dict']
-# print(type(dict))
-# print(dict.keys())
 
-# print("----------learning-------")
 learning = dict['learning']
-# print(type(learning))
-# print(learning.keys())
 
 word_list = []
 form_list = []
 mean_list = []
 sentence_list = []
 
-# print('{:^20}{:^20}{:^20}{:^20}'.format("单词","词性","示意","引句"))
 for word in learning:
     mean = dict['learning'][word]['target']
 
@@ -45,28 +35,7 @@ for word in learning:
     mean_list.append(mean)
     sentence_list.append(sentence)
 
-    # print('{:^20}{:^20}{:^20}{:^20}'.format(word, form, mean, sentence))
 
 dataframe = pd.DataFrame({'单词' :word_list,'词性' :form_list,'示意' :mean_list , '引句' :sentence_list})
 dataframe.to_csv(".//data//learningword(gbk).csv",index=True,sep=',',encoding="gbk")
 dataframe.to_csv(".//data//learningword(utf_8).csv",index=True,sep=',',encoding="utf_8")
-
-# print("----------mastered-------")
-# mastered = dict['mastered']
-# # print(type(mastered))
-# # print(mastered.keys())
-#
-# print('{:^20}{:^20}{:^20}{:^20}'.format("单词","词性","示意","引句"))
-# for word in mastered:
-#     mean = dict['mastered'][word]['target']
-#     if 'form' in dict['mastered'][word]:
-#         form = dict['mastered'][word]['form']
-#
-#     if 'sentence' in  dict['mastered'][word]:
-#         sentence = dict['mastered'][word]['sentence']
-#         sentence = sentence.replace('<b>','<')
-#         sentence = sentence.replace('</b>','>')
-#     else:
-#         sentence = " "
-#
-#     print('{:^20}{:^20}{:^20}{:^20}'.format(word, form, mean, sentence))
